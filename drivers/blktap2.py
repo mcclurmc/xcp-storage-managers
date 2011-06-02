2bf2031b6866ff1e23aa2bff27670876cf0e5874
Andrei Lifchits <andrei.lifchits@citrix.com>
2011-06-02 11:19:08 +0100

    CA-59751: add a copyright & license header to blktap2.py

--- drivers/blktap2.py
+++ drivers/blktap2.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+# Copyright (C) 2010-2011 Citrix Ltd.
+#
+# This program is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published 
+# by the Free Software Foundation; version 2.1 only.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU Lesser General Public License for more details.
+#
+# blktap2: blktap/tapdisk management layer
+#
+
 
 import os
 import re
